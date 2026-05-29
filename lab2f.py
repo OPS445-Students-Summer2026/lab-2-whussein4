4b6aef70382d9bf6c24860b797a77992af4f4ab3
Warsame Hussein <whussein4@myseneca.ca>
2026-05-29 12:04:27 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Warsame Hussein
+# Author ID: whussein4
+# Date Created: 2026/05/29
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
